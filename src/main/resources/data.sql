51ad322cf2d4c419ddc1582fe587ef3c08166c98
Jarosław Jantura <jjantura@gmail.com>
2018-03-01 22:51:00 +0100

    Add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO BOOK(position, title, lastname, firstname, year, language) values(1, 'The Stranger', 'Albert', 'Camus',	1942, 'French');
+INSERT INTO BOOK(position, title, lastname, firstname, year, language) values(2, 'In Search of Lost Time', 'Marcel', 'Proust',	1927, 'French');
