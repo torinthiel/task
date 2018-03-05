af2d205a44cd4014470e6527da678064874339d8
Jarosław Jantura <jjantura@gmail.com>
2018-03-05 22:13:03 +0100

    Make book creation request more reasonable

--- src/main/java/com/example/task/api/BookCreationRequest.java
+++ src/main/java/com/example/task/api/BookCreationRequest.java
@@ -1,5 +1,6 @@
 package com.example.task.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +20,7 @@ public class BookCreationRequest {
     private String title;
 
     @NotNull
+    @JsonProperty("author")
     private AuthorCreationRequest authorCreationRequest;
 
     @NotNull
