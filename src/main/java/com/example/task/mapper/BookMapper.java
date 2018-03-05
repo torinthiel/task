7541f54913846dec364f42080ed35281c3827a7a
Jarosław Jantura <jjantura@gmail.com>
2018-03-05 22:12:32 +0100

    Restore author mapping method

--- src/main/java/com/example/task/mapper/BookMapper.java
+++ src/main/java/com/example/task/mapper/BookMapper.java
@@ -1,6 +1,8 @@
 package com.example.task.mapper;
 
+import com.example.task.api.AuthorCreationRequest;
 import com.example.task.api.BookCreationRequest;
+import com.example.task.model.Author;
 import com.example.task.model.Book;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,5 +14,7 @@ public abstract class BookMapper {
 
     @Mapping(source = "authorCreationRequest", target = "author")
     public abstract Book map(BookCreationRequest request);
+
+    public abstract Author map(AuthorCreationRequest request);
 
 }
