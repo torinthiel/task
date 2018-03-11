d6734cea768cb1a0bdf8d9a49293ccfdba967305
Jarosław Jantura <jjantura@gmail.com>
2018-03-11 12:56:11 +0100

    Fix book imports

--- src/main/java/com/example/task/model/Book.java
+++ src/main/java/com/example/task/model/Book.java
@@ -1,8 +1,17 @@
 package com.example.task.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
