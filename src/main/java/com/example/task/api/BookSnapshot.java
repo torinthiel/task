a8c7e610623a083e4e550a948d9aadd2e2553257
Jarosław Jantura <jjantura@gmail.com>
2018-03-15 21:19:47 +0100

    Remove unnecessary imports from BookSnapshot

--- src/main/java/com/example/task/api/BookSnapshot.java
+++ src/main/java/com/example/task/api/BookSnapshot.java
@@ -1,20 +1,9 @@
 package com.example.task.api;
 
-
-import com.example.task.model.Author;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Data
